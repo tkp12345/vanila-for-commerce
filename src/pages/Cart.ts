b6b1b206509aef38ed9252309ecbfbbe05d2e7a6
roberto <tkp1@hanmail.net>
2024-04-14 04:30:57 +0900

    [feat] : 장바구니 페이지 완성

---
+++ src/pages/Cart.ts
@@ -0,0 +1,79 @@
+import type { Product, StateModel } from '@/react/types/types'
+import Component from '@/react/Component'
+import { router } from '@/react/Roter'
+
+interface CartState extends StateModel {
+  cart: Product[]
+}
+
+export default class Cart extends Component<{}, CartState> {
+  constructor($target: Element) {
+    super($target, {})
+
+    this.state = {
+      cart: JSON.parse(localStorage.getItem('cart') || '[]'),
+    }
+  }
+
+  template() {
+    const { cart } = this.state
+    const cartCount = cart?.length || 0
+    console.log('cartCount:', cartCount)
+    console.log('cart:', cart)
+
+    const cartList =
+      cart &&
+      cart
+        .map(
+          (item) => `
+          <div class='cart-item'>
+            <img src='${item.thumbnail}' alt='${item.title}' />
+            <div class='cart-item-details'>
+              <p>${item.title}</p>
+              <p>${item.quantity}개</p>
+              <button class='remove-item' data-id='${item.id}'>삭제</button>
+            </div>
+          </div>
+        `,
+        )
+        .join('')
+
+    return `
+      <div class='cart-container'>
+        <div class='header-section'>
+          <div class='cart-title-back' >
+            <h1>◀ Cart</h1>
+          </div>
+          <div class="cart-title">장바구니 <span>${cartCount}</span> 개</div>
+        </div>
+           <div class="cart-item-section">
+          ${cartCount ? cartList : `<div>장바구니가 비어있습니다</div>`}
+          </div>
+      </div>
+    `
+  }
+
+  setEvent() {
+    this.addEvent('click', '.cart-title-back', () => {
+      router.back()
+    })
+
+    this.addEvent('click', '.cart-title', () => {
+      router.push(`/cart`)
+    })
+
+    this.addEvent('click', '.remove-item', (event: MouseEvent) => {
+      const target = event.target as HTMLElement
+      const itemId = target.dataset.id
+      if (!itemId) return
+      this.removeItem(itemId)
+    })
+  }
+
+  removeItem(itemId: string) {
+    const { cart } = this.state
+    const newCart = cart.filter((item) => item.id.toString() !== itemId)
+    this.setState({ cart: newCart })
+    localStorage.setItem('cart', JSON.stringify(newCart))
+  }
+}
